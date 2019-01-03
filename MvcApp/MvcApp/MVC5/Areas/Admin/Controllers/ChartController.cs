2481b9f847b9d816bfae70ddd8d37dfa445b5f57
Milap Thakkar <milapthakkar@users.noreply.github.com>
2019-01-04 02:22:15 +0530

    MVC5 SB Admin theme integration
    MVC5 SB Admin theme integration

---
+++ MvcApp/MvcApp/MVC5/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Admin/Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcApp/MvcApp/MvcApp/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult RedirecRegister()
+        {
+            return RedirectToAction("Index", "register");
+        }
+
+    }
+}
